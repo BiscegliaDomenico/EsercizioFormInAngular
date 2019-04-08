91db4b417602b1705ae17a30357193ef0e210cd5
BiscegliaDomenico <bisceglia.domenico@iisgalvanimi.edu.it>
2019-04-08 06:57:16 +0000

    primo commit

---
+++ form-demo/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormDemoComponent } from './form-demo/form-demo.component';
+
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormDemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    NgbModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
